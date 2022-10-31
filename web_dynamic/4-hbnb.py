ef921517dd585530fa3dad25d7012e066e587c50
[chrpkorir] <faythkoko@gmail.com>
2022-10-31 17:28:27 +0300

    Update

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 
 #begin flask page rendering
 @app.teardown_appcontext
-def close_db(error):
+def close_db(exception):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all('State').values()
@@ -31,7 +31,7 @@ def hbnb():
     places = storage.all('Place').values()
     places = sorted(places, key=lambda k: k.name)
     cache_id = uuid.uuid4()
-    return render_template('0-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
